232b7d16bd068083babca862dec152272635e629
cMoi <cMonEmail@mail.com>
2023-03-02 23:57:53 +0100

    initial commit

---
+++ Scrutin TP2/ScrutinTest/Features/Scrutin.feature
@@ -0,0 +1,47 @@
+﻿Feature: Scrutin
+
+Scenario: Voting not closed
+	Given candidat 1 has 1 votes
+	And candidat 2 has 2 votes
+	When get winner
+	Then exception 'Sondage non cloturé' is thrown
+
+Scenario: Voting 1 round
+	Given candidat 1 has 1 votes
+	Given candidat 2 has 2 votes
+	When the voting is closed
+	Then winner is candidat 2
+
+Scenario: Get votes
+	Given candidat 1 has 0 votes
+	And candidat 2 has 2 votes
+	Then candidat 1 has 0 votes and 0%
+	And candidat 2 has 2 votes and 100%
+
+Scenario: Voting 2 rounds
+	Given candidat 1 has 2 votes
+	And candidat 2 has 0 votes
+	And candidat 3 has 2 votes
+	When the voting is closed
+	Then candidat 1 is in vote
+	And candidat 1 has 0 votes
+	And candidat 2 is not in vote
+	And candidat 3 is in vote
+	And candidat 3 has 0 votes
+
+Scenario: Voting 2 rounds with winner
+	Given it is second round
+	Given candidat 1 has 2 votes
+	And candidat 2 has 0 votes
+	When the voting is closed
+	Then winner is candidat 1
+
+Scenario: Voting 2 round with egality
+	Given it is second round
+	And candidat 1 has 2 votes
+	And candidat 2 has 0 votes
+	When the voting is closed
+	Then it is an egality
+
+	
+	
---
+++ Scrutin TP2/scrutin/Scrutin.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+namespace scrutin;
+
+public class Scrutin
+{
+    public bool isSecondRound = false;
+    private int winner;
+    private bool isClosed = false;
+    private bool isEgalite = false;
+    private Dictionary<int, int> votes = new Dictionary<int, int>();
+    public void setCandidat(int candidat, int nbVotes)
+    {
+        if (isClosed) throw new Exception("Sondage cloturé");
+        votes.Add(candidat, nbVotes);
+    }
+
+    public void close()
+    {
+        isClosed = true;
+
+        // Si c'est le premier tour, chercher le gagnant ou les deux candidats ayant le plus de votes
+        if (!isSecondRound)
+        {
+            int nbVotesTotal = votes.Values.Sum();
+            winner = votes.Keys.FirstOrDefault(x => (double)votes[x] / nbVotesTotal > 0.5);
+
+            if (winner == 0)
+            {
+                var topTwoCandidates = votes.OrderByDescending(x => x.Value).Take(2).Select(x => x.Key).ToList();
+                votes.Clear();
+                votes.Add(topTwoCandidates[0], 0);
+                votes.Add(topTwoCandidates[1], 0);
+                isSecondRound = true;
+            }
+        }
+        // Si c'est le deuxième tour, chercher le gagnant ou indiquer une égalité
+        else
+        {
+            int nbVotesTotal = votes.Values.Sum();
+            winner = votes.Keys.FirstOrDefault(x => (double)votes[x] / nbVotesTotal > 0.5);
+            if (winner == 0) isEgalite = true;
+        }
+    }
+
+    public int getPourcentage(int candidat)
+    {
+        int totalVotes = votes.Values.Sum();
+        if (totalVotes == 0) return 0;
+        return (votes[candidat] * 100) / totalVotes;
+    }
+
+    public int getVotes(int candidat)
+    {
+        return votes[candidat];
+    }
+
+    public bool isInVote(int candidat)
+    {
+        return votes.ContainsKey(candidat);
+    }
+
+    public void setSecondRound()
+    {
+        isSecondRound = true;
+    }
+
+    public int getWinner()
+    {
+        if (!isClosed) throw new Exception("Sondage non cloturé");
+        if (isEgalite) throw new Exception("Egality");
+        return winner;
+
+    }
+}
